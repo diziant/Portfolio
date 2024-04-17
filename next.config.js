8eb0404209e4d003660d5dbd9c7448461e23581c
diziant <folomeev-egor@mail.ru>
2024-04-17 16:50:04 +0000

    dev: setup deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 module.exports = {
   trailingSlash: true,
+  basePath: '/Portfolio',
+  output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   }
